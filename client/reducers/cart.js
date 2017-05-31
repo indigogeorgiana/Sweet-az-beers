368155fe55bb297caf1b74ddc2b8e638dea81586
Don Smith <don@antiantonym.com>
2017-05-31 16:43:06 +1200

    increment cart item if exists

--- client/reducers/cart.js
+++ client/reducers/cart.js
@@ -3,12 +3,27 @@ import {ADD_TO_CART} from '../actions'
 const cart = (state = [], action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      return [
-        ...state,
-        {id: action.id, quantity: 1} // TODO: increment if already added
-      ]
+      return getNewCart(state, action.id)
     default:
       return state
+  }
+}
+
+function getNewCart (state, id) {
+  let exists = false
+  const newState = state.map(item => {
+    if (item.id === id) {
+      item.quantity += 1
+      exists = true
+    }
+    return item
+  })
+
+  if (exists) {
+    return newState
+  } else {
+    newState.push({id: id, quantity: 1})
+    return newState
   }
 }
 
